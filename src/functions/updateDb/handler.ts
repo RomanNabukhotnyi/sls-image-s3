8ad2dd85dc53dd868c6d86bbe66855ec43cd194c
RomanNabukhotnyi <97584054+RomanNabukhotnyi@users.noreply.github.com>
2022-06-07 18:40:20 +0300

    Update handler.ts

--- src/functions/updateDb/handler.ts
+++ src/functions/updateDb/handler.ts
@@ -2,8 +2,8 @@ import { middyfy } from '../../libs/lambda';
 import { Dynamo } from '../../libs/db';
 
 const updateDb = async (event) => {
-    const key = event.Records[0].s3.object.key;
-    const email = decodeURIComponent(key.split('/')[0]);
+    const key = decodeURIComponent(event.Records[0].s3.object.key);
+    const email = key.split('/')[0];
 
     const file = {
         key,
