3c82f7d410d2249ae953d4b40a7eb5bdc9f6a03c
RomanNabukhotnyi <97584054+RomanNabukhotnyi@users.noreply.github.com>
2022-06-08 12:12:29 +0300

    Update schema.ts

--- src/functions/signup/schema.ts
+++ src/functions/signup/schema.ts
@@ -13,10 +13,3 @@ export default {
     },
     required: ['email', 'password'],
 } as const;
-
-// import joi from 'joi';
-
-// export default joi.object({
-//     email: joi.string().email().max(100).required(),
-//     password: joi.string().min(6).max(15).disallow(' ', '\"', '\'').required(),
-// });
